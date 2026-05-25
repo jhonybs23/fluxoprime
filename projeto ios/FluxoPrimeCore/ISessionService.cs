9bbc429ed3352ccda53982a54912bf4b4643ddbc
jhonybs23 <57757954+jhonybs23@users.noreply.github.com>
2026-05-24 23:40:24 -0300

    Delete projeto ios directory

--- projeto ios/FluxoPrimeCore/ISessionService.cs
+++
@@ -1,9 +0,0 @@
-namespace FluxoPrimeCore;
-
-public interface ISessionService
-{
-    Task SaveSessionAsync(string username, string password, string baseUrl);
-    Task<(string Username, string Password, string BaseUrl)> LoadSessionAsync();
-    Task ClearSessionAsync();
-    Task<bool> HasSessionAsync();
-}
